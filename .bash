57bfc105dbf2fce862eced07dd2ffb055664a5d1
gnlehc <chelseang.cn@gmail.com>
2024-06-02 18:18:10 +0700

    feat: Get and Set Value

---
+++ .bash
@@ -0,0 +1,6 @@
+# 1. Stop Redis Server
+sudo service redis-server stop
+# 2. Start Redis Server
+redis-server
+# 3. Redis CLI
+redis-cli
--- main.go
+++ main.go
@@ -24,6 +24,23 @@ func main() {
 	}
 	fmt.Printf("Ping response: %s\n", ping)
 
+	// Set a value in Redis
+	err = client.Set(context.Background(), "name", "lotso", 0).Err()
+	if err != nil {
+		fmt.Printf("Failed to set value in the Redis instance: %s\n", err.Error())
+		return
+	}
+	fmt.Println("Value 'lotso' set in Redis")
+
+	// Get the value from Redis
+	val, err := client.Get(context.Background(), "name").Result()
+	if err != nil {
+		fmt.Printf("Failed to get value from Redis: %s\n", err.Error())
+		return
+	}
+	fmt.Printf("Value from Redis: %s\n", val)
+
+	// model payload
 	jsonString, err := json.Marshal(&model.Person{
 		Name:  "Chelsea",
 		Age:   19,
@@ -43,12 +60,11 @@ func main() {
 	fmt.Printf("Value %s set in Redis", jsonString)
 
 	// Get the value from Redis
-	val, err := client.Get(context.Background(), "person").Result()
+	vals, err := client.Get(context.Background(), "person").Result()
 	if err != nil {
 		fmt.Printf("Failed to get value from Redis: %s\n", err.Error())
 		return
 	}
 	fmt.Println()
-	fmt.Printf("Value from Redis: %s\n", val)
-
+	fmt.Printf("Value from Redis: %s\n", vals)
 }
